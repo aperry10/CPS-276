ac82b71d9f77c892c4c1dcbfff342eaeb3d44a82
Amanda Perry-Rienstra <aperry10@russet-v8.wccnet.edu>
2026-09-16 10:31:06 -0400

    Add assignment 1

--- phpmysqltest/phpmysqltest/classes/Db_conn.php
+++ phpmysqltest/phpmysqltest/classes/Db_conn.php
@@ -9,9 +9,9 @@ class DatabaseConn {
     try {
 
       $dbHost = 'localhost';
-      $dbName = 'aperry10';//this will be your WCC username
-      $dbUsr = 'aperry10';//this will be your WCC username
-      $dbPass = 'OBHX5Iup9sEA7sx';//this will be the password found in the video.
+      $dbName = 'WCC Username';//this will be your WCC username
+      $dbUsr = 'WCC Username';//this will be your WCC username
+      $dbPass = 'Your password';//this will be the password found in the video.
 
       $this->conn = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUsr, $dbPass);
       
